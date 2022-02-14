4d340a27685b509effabc022b6cd5b8a22359309
LilasCorner <59840908+LilasCorner@users.noreply.github.com>
2022-02-14 13:36:50 -0600

    new generator exploration

--- src/jcrypto/Coins.java
+++ src/jcrypto/Coins.java
@@ -5,8 +5,10 @@ package jcrypto;
 
 import java.util.Random;
 
+import cern.jet.random.Uniform;
 import repast.simphony.engine.environment.RunEnvironment;
 import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.random.RandomHelper;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.continuous.NdPoint;
 import repast.simphony.space.grid.Grid;
@@ -22,6 +24,11 @@ public class Coins {
 	private ContinuousSpace<Object> space; 
 	private Grid<Object> grid;
 	private Random rnd = new Random();
+	
+	private double MaxPriceIncrease = 1.05;
+	private double MinPriceIncrease = 0.95;
+	
+	private Uniform unigen = RandomHelper.createUniform(MinPriceIncrease, MaxPriceIncrease);
 	
 	//default constructor
 	public Coins(String name, ContinuousSpace < Object > space , Grid < Object > grid, double value){
@@ -63,14 +70,15 @@ public class Coins {
 	//coin value calculated as time passes
 	public void age() {	
         
-		double ri = rnd.nextDouble();
+		
+		double ri = unigen.nextDouble();
 		double coinPrice = this.value;
 		double oldPrice  = this.value;
 		int smallScale = 0;
 		
 		NdPoint myPoint = space.getLocation (this);
 		
-		ri = ((ri * .1)-.05)+1;
+		//ri = ((ri * .1)-.05)+1;
 		 
 		
 		coinPrice = coinPrice * ri; 
@@ -78,7 +86,7 @@ public class Coins {
 		smallScale = (int)( oldPrice - coinPrice )/10; //scale down # to show on grid
 		
 		//move coin vertically based on new price, X axis static
-		space.moveByDisplacement(this, 0, smallScale); //x, y displacement
+		space.moveByDisplacement(this, 1, smallScale); //x, y displacement
 		myPoint = space.getLocation(this);
 		grid.moveTo(this, (int)myPoint.getX(), (int)myPoint.getY());
 		
